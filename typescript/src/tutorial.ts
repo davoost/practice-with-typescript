1cdce1c8d05b79ba8be666945d1ffd2c2fbff529
David Oosthuizen <david.oosthuizen@hotmail.com>
2024-06-20 16:52:33 +0100

    worked more on typescript tutorial. used the alias and interface functions to create a model for types. Feels a bit like creating a class in python

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -34,7 +34,7 @@ let fruit: string[] = ['apple', 'orange', 'banana']
 
 let emptyValues: any[] = ['hello']
 
-let names = ['peter', 'susan', 1]
+let names2 = ['peter', 'susan', 1]
 let array:(string | boolean)[] = ['apple', true]
 
 let temperature: number[] = [45, 30, 20]
@@ -61,3 +61,251 @@ let laptop2: { brand:string; year?: number } = { brand:'Dell' }
 let product1: {title:string; price: number} = {title: 'shirt', price:20}
 let product2: {title:string;} = {title: 'pants'}
 let products: {title:string; price?: number}[] = [product1, product2]
+
+
+function sayHi(name:string){
+    console.log(`Hello there ${name.toUpperCase()}`)
+}
+// - any
+// - config
+// - declare type - string
+
+sayHi('David')
+// sayHi(3) this is an error
+
+function calculateDiscount(price:number){
+    return price * 0.9
+}
+//type script infers the type of finalPrice
+const finalPrice = calculateDiscount(200)
+
+let names:string[] = ['John', 'Peter', 'Mark']
+function isNameHere(name:string):boolean{
+    if(names.includes(name)){
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+const isMark = isNameHere('Harry')
+console.log(isMark)
+
+function calculatePrice(price:number, discount?:number){
+    return price - (discount || 0) //can setup a default parameter if not defined
+}
+
+let priceAfterDiscount = calculatePrice(100, 20)
+
+// can also setup a default value here
+function calculateScore(initialScore: number, penaltyPoints:number = 0):number{
+    return initialScore - penaltyPoints
+}
+
+let scoreAfterPenalty = calculateScore(100, 20)
+let scoreWithoutPenalty = calculateScore(300)
+//thre dots is treated as an array
+function sum(message: string, ...numbers:number[]): string {
+    const doubled = numbers.map((num) => num * 2);
+    console.log(doubled)
+
+    let total = numbers.reduce((previous, current) => {
+        return previous + current;
+    }, 0)
+    return `${message} ${total}`
+}
+
+let firstResult = sum('The total is : ', 1, 2, 3, 4, 5)
+console.log(firstResult)
+
+function logMessage(message:string):void{
+    console.log(message)
+}
+
+logMessage('Hello, Typescript')
+
+function processInput(number: number | string) {
+    let output = typeof number === 'number' ? number*2 : number.toUpperCase()
+    console.log(output)
+}
+processInput('5')
+
+function createEmployee({ id }:{ id:number }):{
+    id: number;
+    isActive:boolean} 
+    //the type of each element of the object is set
+    {
+    return {id, isActive: id % 2 === 0}
+}
+
+const first = createEmployee({id:1})
+const second = createEmployee({id:2})
+console.log(first, second)
+
+//alternative approach
+
+function createStudent(student:{id:number;name:string}):void{
+    console.log(`Welcome to the course ${student.name.toUpperCase()}`)
+}
+
+const newStudent = {
+    id: 5,
+    name: 'anna',
+    email: 'anna@hotmail.com'
+}
+
+createStudent(newStudent)
+// when object properties are passed inline, typescript will complain
+// createStudent({id:1, name:'bob', email:'bobo@gmail.com'})
+
+
+function processData(input:string|number, config:{reverse:boolean} = 
+    {reverse:false}):string | number{
+    if(typeof input === 'number'){
+        return input*input
+    }
+    else{
+        return config.reverse ? input.toUpperCase().split('').reverse().join('') : input.toUpperCase()
+    }
+}
+
+console.log(processData(10));
+console.log(processData('Hello'));
+console.log(processData('Hello', {reverse:true}));
+
+type User = { id: number; name: string; isActive: boolean }
+
+const john: User = {
+    id: 1,
+    name: "john",
+    isActive: true,
+  };
+  const susan: User = {
+    id: 1,
+    name: "susan",
+    isActive: false,
+  };
+  
+  function createUser(user: User): {
+    id: number;
+    name: string;
+    isActive: boolean;
+  } {
+    console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+  
+    return user;
+  }
+
+  type stringOrNumber = string | number
+
+  let value: stringOrNumber;
+  value = 'hello'
+  value = 123
+
+  type Theme = 'light' | 'dark'
+
+  let theme: Theme;
+  theme = 'dark'
+  theme = 'light'
+
+  function setTheme(t: Theme){
+    theme = t
+  }
+
+  setTheme('dark')
+
+  type Employee = {id:number, name:string, department: string}
+  type Manager = {id:number, name:string, employees: Employee[]}
+type Staff = Employee | Manager
+
+  function printStaffDetails(staff:Staff):void{
+    if('employees' in staff){
+        console.log(`This person is a manager and has ${staff.employees.length}`)
+    }
+    else {
+        console.log(`This person is an employee in department ${staff.department}`)
+    }
+  }
+
+  const alice:Employee = {id:1, name: 'alice', department: 'Sales'}
+  const steve:Employee = {id:1, name: 'steve', department: 'HR'}
+  const bob:Manager = {id:1, name: 'bobo', employees: [alice, steve]}
+  printStaffDetails(alice)
+  printStaffDetails(steve)
+  printStaffDetails(bob)
+
+  type Book = {id:number; name: string; price: number};
+  type DiscountedBook = Book &{ discount: number }
+
+  const book1:Book = {
+    id:1,
+    name: 'how to cook a dragon',
+    price: 15
+  };
+  const book2:Book = {
+    id:2,
+    name: 'the secret life of unicorns',
+    price: 18
+  }
+  // this below alows you to use properties of other types then add on more more
+  const discountedBook:Book&{discount: number} = {
+    id:3,
+    name: 'Gnomes vs. Goblins: The Ultimate Guide',
+    price: 25,
+    discount: 0.15
+  }
+const propName = 'age'
+
+// can use computed properties in variable names (like javasript)
+type Animal = {
+    [propName] : number;
+}
+
+  let tiger = { [propName]:5 } 
+
+  interface OtherBook {
+    readonly isbn: number;
+    title: string;
+    author: string;
+    genre?: string;
+    // method
+    printAuthor():void;
+    printTitle(message: string): string;
+    printSomething:(someValue:number)=>number;
+  }
+
+  const deepWork: OtherBook = {
+    isbn: 123,
+    title: 'deep work',
+    author: 'Cal Newport',
+    genre: 'self-help',
+    printAuthor(){
+        console.log(this.author)
+    },
+    printTitle(message){
+        return `${this.title} ${message}`;
+    },
+    // first option (maybe preferable)
+    // printSomething: function(someValue){
+    //     return someValue
+    // }
+    // second option
+    // printSomething: (someValue) => {
+    //     console.log(deepWork.author)
+    //     return someValue;
+    // }
+    // third option
+    printSomething(someValue){
+        console.log(this.title)
+        return someValue
+    }
+    // fourth option
+  }
+
+//   deepWork.isbn = 'some value'
+deepWork.printAuthor();
+const result = deepWork.printTitle("is an awesome book");
+console.log(result);
+
+console.log(deepWork.printSomething(34))
