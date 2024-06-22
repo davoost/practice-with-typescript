4c3bcc18ded871228ab8c06484f3cf7cd956d0b6
David Oosthuizen <david.oosthuizen@hotmail.com>
2024-06-22 17:04:45 +0100

    next part of tutorial on type guards

--- typescript/src/tutorial-3.ts
+++ typescript/src/tutorial-3.ts
@@ -1,153 +1,311 @@
-// let person:[string, number] = ['john', 25] // fixed length and fixed types
+// // // let person:[string, number] = ['john', 25] // fixed length and fixed types
 
-// let date: [number, number, number] = [12, 17, 2001]
+// // // let date: [number, number, number] = [12, 17, 2001]
 
-// function getPerson():[string, number]{
-//     return['john', 25];
-// }
-// let randomPerson = getPerson()
-// console.log(randomPerson[0])
-// console.log(randomPerson[1])
+// // // function getPerson():[string, number]{
+// // //     return['john', 25];
+// // // }
+// // // let randomPerson = getPerson()
+// // // console.log(randomPerson[0])
+// // // console.log(randomPerson[1])
 
-// let susan: [string, number?] = ['susan']
+// // // let susan: [string, number?] = ['susan']
 
-// enum ServerResponseStatus {
-//     Success = 'Success',
-//     Error = 'Error'
-// }
+// // // enum ServerResponseStatus {
+// // //     Success = 'Success',
+// // //     Error = 'Error'
+// // // }
 
-// // Object.values(ServerResponseStatus).forEach((value)=>{
-// //     if(typeof value === 'number'){
-// //         console.log(value)
+// // // // Object.values(ServerResponseStatus).forEach((value)=>{
+// // // //     if(typeof value === 'number'){
+// // // //         console.log(value)
+// // // //     }
+// // // // })
+
+// // // console.log(ServerResponseStatus)
+
+// // // interface ServerResponse{
+// // //     result:ServerResponseStatus;
+// // //     data: string[]
+// // // }
+
+// // // function getServerResponse():ServerResponse{
+// // //     return {
+// // //         result: ServerResponseStatus.Success,
+// // //         data: ['first item', 'second item']
+// // //     }
+// // // }
+
+// // // const response: ServerResponse = getServerResponse()
+// // // console.log(response)
+
+// // // enum UserRole {
+// // //     Admin, 
+// // //     Manager, 
+// // //     Employee
+// // // }
+
+// // // type User = {id:number; name:string; role:UserRole; contact:[string, string]}
+
+// // // function createUser(user:User):User{
+// // //     return user;
+// // // }
+
+// // // const user:User = createUser({
+// // //     id: 1, name: 'john doe', role: UserRole.Admin, contact:['john@gmail.com', '123456-789']
+// // // })
+// // // console.log(user)
+
+// // // let someValue: any = 'this is a string'
+
+// // // let strLength: number = ( someValue as string ).length
+
+// // // type Bird = {
+// // //     name: string
+// // // }
+
+// // // let birdString = '{"name": "Eagle"}';
+// // // let dogString = '{"breed": "Poodle"}';
+
+// // // let birdObject = JSON.parse(birdString)
+// // // let dogObject = JSON.parse(dogString)
+
+// // // let bird = birdObject as Bird
+// // // let dog = dogObject as Bird
+
+// // enum Status{
+// //     Pending = 'pending',
+// //     Declined = 'declined',
+// // }
+
+// // type User = {
+// //     name: string,
+// //     status: Status
+// // }
+
+// // // save Status.Pending in the DB as a string
+// // // retrieve string from the DB
+
+// // const statusValue = 'pending'
+// // const user:User = {name: 'john', status:statusValue as Status}
+
+// // let unknownValue: unknown;
+
+// // unknownValue = 'hello world'
+// // unknownValue = [1,2,3]
+// // unknownValue = 42.33456
+
+// // if(typeof unknownValue === 'number'){
+// //     unknownValue.toFixed(2)
+// // }
+
+// // try { 
+
+// //     throw new Error('there was an error....')
+
+// // } catch(error){
+// //     if(error instanceof Error){
+// //         console.log(error.message)
 // //     }
-// // })
+// // }
 
-// console.log(ServerResponseStatus)
+// // let someValue: never // cannot assign any value to type never
 
-// interface ServerResponse{
-//     result:ServerResponseStatus;
-//     data: string[]
+// // enum Colour {
+// //     Red, 
+// //     Blue,
+// //     Green,
+// // }
+
+// // function getColourName(colour:Colour){
+// //     switch(colour) {
+// //         case Colour.Red:
+// //             return 'Red';
+// //         case Colour.Blue:
+// //             return 'Blue'
+// //         case Colour.Green:
+// //             return 'Green'
+// //         default:
+// //             // at build time
+// //             let unexpectedColour: never = colour
+// //             // at runtime
+// //             throw new Error(`Unexpected colour value: ${colour}`)
+// //     }
+// // }
+// // console.log(getColourName(Colour.Red))
+// // console.log(getColourName(Colour.Blue))
+// // console.log(getColourName(Colour.Green))
+
+// // let something = 'susan'
+
+// // import newStudent, {sayHello, person, type Student} from './actions'
+
+// // sayHello('Typescript')
+// // console.log(person)
+// // console.log(newStudent)
+
+// // const anotherStudent:Student = {
+// //     name: 'bob', 
+// //     age: 30}
+
+// //     console.log(anotherStudent)
+
+// // // import { someValue } from './example.js' // typescript can't import js files
+
+// type ValueType = string | number | boolean;
+
+// let value: ValueType;
+// const random = Math.random();
+// value = random < 0.33 ? "Hello" : random < 0.66 ? 123.456 : true;
+
+// function checkValue(value:ValueType):void{
+//     if(typeof value === 'string'){
+//         console.log(value.toLowerCase())
+//         return;
+//     }
+//     else if(typeof value === 'number'){
+//         console.log(Math.round(value*100)/100)
+//         return;
+//     }
+//     else {
+//         console.log('boolean: '+value)
+//         return;
+
+//     }
 // }
+// checkValue(value)
 
-// function getServerResponse():ServerResponse{
-//     return {
-//         result: ServerResponseStatus.Success,
-//         data: ['first item', 'second item']
+type Dog = { type: "dog"; name: string; bark: () => void };
+type Cat = { type: "cat"; name: string; meow: () => void };
+type Animal = Dog | Cat;
+
+//check for a property or a method
+
+// function makeSound(animal:Animal):void{
+//     if(animal.type === 'dog'){
+//         animal.bark()
+//     }
+//     else {
+//         animal.meow()
 //     }
 // }
 
-// const response: ServerResponse = getServerResponse()
-// console.log(response)
-
-// enum UserRole {
-//     Admin, 
-//     Manager, 
-//     Employee
+// function makeSound(animal:Animal){
+//     if('bark' in animal){
+//         animal.bark()
+//     }
+//     else {
+//         animal.meow()
+//     }
 // }
 
-// type User = {id:number; name:string; role:UserRole; contact:[string, string]}
+// function printLength(str:string | null | undefined):void{
 
-// function createUser(user:User):User{
-//     return user;
+//     if(str){
+//         console.log(str.length)
+//     }
+//     else {
+//         console.log('no string provided')
+//     }
 // }
 
-// const user:User = createUser({
-//     id: 1, name: 'john doe', role: UserRole.Admin, contact:['john@gmail.com', '123456-789']
-// })
-// console.log(user)
+// printLength(null)
+// printLength(undefined)
+// printLength('')
+// printLength('a string')
 
-// let someValue: any = 'this is a string'
-
-// let strLength: number = ( someValue as string ).length
-
-// type Bird = {
-//     name: string
+// type script might complain with try and catch as we might have an error that isn't 
+// an instance of the error class.
+// try {
+//     throw 'some error'
+//     // throw new Error('This is an error')//
+// } catch(error){
+//     if(error instanceof Error){
+//         console.log(`Caught an error object: ${error.message}`)
+//     }
+//     else {
+//         console.log('unknown error...')
+//     }
 // }
 
-// let birdString = '{"name": "Eagle"}';
-// let dogString = '{"breed": "Poodle"}';
+// function checkInput(input:Date | string):string{
+//     if(input instanceof Date){
+//         return input.getFullYear().toString()
+//     }
+//     return input
+// }
 
-// let birdObject = JSON.parse(birdString)
-// let dogObject = JSON.parse(dogString)
+// const year = checkInput(new Date());
+// const random = checkInput('2020-05-05')
 
-// let bird = birdObject as Bird
-// let dog = dogObject as Bird
+// console.log(year)
+// // console.log(random)
 
-enum Status{
-    Pending = 'pending',
-    Declined = 'declined',
-}
+// type Student = {
+//     name: string;
+//     study: () => void;
+//   };
+  
+//   type User = {
+//     name: string;
+//     login: () => void;
+//   };
+  
+//   type Person = Student | User;
+  
+//   const randomPerson = (): Person => {
+//     return Math.random() > 0.5
+//       ? { name: "john", study: () => console.log("Studying") }
+//       : { name: "mary", login: () => console.log("Logging in") };
+//   };
+  
+//   const person = randomPerson();
 
-type User = {
-    name: string,
-    status: Status
-}
+//   function isStudent(person:Person):person is Student{
+//         //return 'study' in person
+//         return (person as Student).study !== undefined
+//   }
 
-// save Status.Pending in the DB as a string
-// retrieve string from the DB
+//   if(isStudent(person)){
+//     person.study()
+//   }
+//   else {
+//     person.login()
+// //   }
 
-const statusValue = 'pending'
-const user:User = {name: 'john', status:statusValue as Status}
+// type IncrementAction = {
+//     type: 'increment',
+//     amount: number;
+//     timestamp: number;
+//     user: string;
+//   };
+  
+//   type DecrementAction = {
+//     type: 'decrement',
+//     amount: number;
+//     timestamp: number;
+//     user: string;
+//   };
+  
+//   type Action = IncrementAction | DecrementAction;
 
-let unknownValue: unknown;
+// function reducer (state:number, action:Action){
+//     switch(action.type){
+//         case 'increment':
+//             return state + action.amount
+//         case 'decrement':
+//             return state - action.amount
+//         default: 
+//         const unexpectedAction: never = action
+//             throw new Error(`Unexpected action: ${unexpectedAction}`)
+//     }
+// }
 
-unknownValue = 'hello world'
-unknownValue = [1,2,3]
-unknownValue = 42.33456
+//   const newState = reducer(15, {
+//     user:'John',
+//     type: 'decrement',
+//     amount: 20,
+//     timestamp:123456
+//   })
 
-if(typeof unknownValue === 'number'){
-    unknownValue.toFixed(2)
-}
-
-try { 
-
-    throw new Error('there was an error....')
-
-} catch(error){
-    if(error instanceof Error){
-        console.log(error.message)
-    }
-}
-
-let someValue: never // cannot assign any value to type never
-
-enum Colour {
-    Red, 
-    Blue,
-    Green,
-}
-
-function getColourName(colour:Colour){
-    switch(colour) {
-        case Colour.Red:
-            return 'Red';
-        case Colour.Blue:
-            return 'Blue'
-        case Colour.Green:
-            return 'Green'
-        default:
-            // at build time
-            let unexpectedColour: never = colour
-            // at runtime
-            throw new Error(`Unexpected colour value: ${colour}`)
-    }
-}
-console.log(getColourName(Colour.Red))
-console.log(getColourName(Colour.Blue))
-console.log(getColourName(Colour.Green))
-
-let something = 'susan'
-
-import newStudent, {sayHello, person, type Student} from './actions'
-
-sayHello('Typescript')
-console.log(person)
-console.log(newStudent)
-
-const anotherStudent:Student = {
-    name: 'bob', 
-    age: 30}
-
-    console.log(anotherStudent)
-
-// import { someValue } from './example.js' // typescript can't import js files
+//   console.log(newState)
